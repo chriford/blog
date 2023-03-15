823939c8de53252e98b188c7fc8d0fc699eace4b
chriford <siamechrif@gmail.com>
2023-03-15 18:57:45 -0400

    create post form template, create post create view

--- blog/forms/post.py
+++ blog/forms/post.py
@@ -13,29 +13,29 @@ from crispy_forms.layout import (
 )
 
 class PostForm(ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = ''
-        self.helper.form_id = 'post-create-form-id'
-        self.helper.layout = Layout(
-            Column('body', css_class="col-md-12 col-md-12 col-12 col-sm-12"),
-            Row(
-                Field('title', css_class="col-md-6 col-lg-6 col-sm-6"),
-                Field('category', css_class="col-md-6 col-lg-6 col-sm-6"),
-            ),
-            Column('body', css_class="col-md-12 col-sm-12"),
-            Div(
-                Submit('submit', 'create post', 
-                    css_class='btn btn-success d-flex w-100 justify-content-center',
-                    css_id="post-create-id",
-                ),
-                css_class="d-flex justify-content-center py-0 pb-1",
-            )
-        )
-
     class Meta:
         model = Post
         fields = ['owner', 'title', 'category', 'body',]
+
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.helper = FormHelper()
+            self.helper.form_class = 'bg-white'
+            self.helper.form_id = 'post-create-form-id'
+            self.helper.layout = Layout(
+                Column('body', css_class="col-md-12 col-md-12 col-12 col-sm-12"),
+                Row(
+                    Field('title', css_class="col-md-6 col-lg-6 col-sm-6"),
+                    Field('category', css_class="col-md-6 col-lg-6 col-sm-6"),
+                ),
+                Column('body', css_class="col-md-12 col-sm-12"),
+                Div(
+                    Submit('submit', 'create post', 
+                        css_class='btn btn-success d-flex w-100 justify-content-center',
+                        css_id="post-create-id",
+                    ),
+                    css_class="d-flex justify-content-center py-0 pb-1",
+                )
+            )
 
--- blog/views/posts.py
+++ blog/views/posts.py
@@ -16,10 +16,27 @@ from blog.models import (
 )
 from blog.forms import PostForm
 
+
+@login_required(login_url=settings.LOGIN_REDIRECT_URL)
+def post_create(request):
+    if request.method == 'POST':
+        post_create_form = PostForm(request.POST)
+        if post_create_form.is_valid():
+            post = post_create_form.save(commit=False)
+            post.cleaned_data['owner'] = request.user
+            post.save(commit=True)
+            return redirect('blog:post-create')
+            
+    posts = Post.objects.all()
+    context = {
+        'post_form': PostForm,
+        'posts': posts,
+    }
+    return render(request, 'blog/posts.html', context)
+
 @login_required(login_url=settings.LOGIN_REDIRECT_URL)
 def posts(request):
     posts = Post.objects.all()
-
     context = {
         'post_form': PostForm,
         'posts': posts,
