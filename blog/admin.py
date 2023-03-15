0b85858bcdf86a53fc15778bdf08030ee4e6a355
chriford <siamechrif@gmail.com>
2023-03-15 02:52:22 -0400

    register post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from blog.models import (
     Category,
+    Post,
 )
 
 @admin.register(Category)
@@ -8,4 +9,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name','created_at','updated_at']
     search_fields = ['name',]
     list_per_page = 20
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['owner','title', 'created_at','updated_at']
+    search_fields = ['title','owner']
+    list_per_page = 25
 
