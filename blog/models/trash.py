be5dda007aa38bab9b8af19e14953a2ff4e9c287
chriford <siamechrif@gmail.com>
2023-03-15 15:55:48 -0400

    modify the trash model

--- blog/models/trash.py
+++ blog/models/trash.py
@@ -8,12 +8,40 @@ from blog.models.timestamp import Timestamp
 
 
 class Trash(Timestamp):
-    post = models.ForeignKey(
-        verbose_name=_("Post"),
-        to='blog.Post',
+    post_id = models.PositiveIntegerField(
+        null=True,
+        blank=False
+    )
+    owner = models.ForeignKey(
+        'security.User',
+        related_name='user',
+        help_text=_("The owner of this post."),
+        null=True,
+        blank=True,
         on_delete=models.CASCADE,
+    )
+    title = models.CharField(
+        max_length=200,
+        verbose_name=_("Title"),
+        help_text=_("Title of the post"),
         null=True,
         blank=False,
+    )
+    category = models.ForeignKey(
+        'blog.Category',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        help_text=_("Select the category of this post"),
+    )
+    body = models.TextField(
+        verbose_name=_("Body"),
+        null=True,
+    )
+    restore = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(
+        auto_now_add=True,
+        
     )
     
     def __str__(self):
