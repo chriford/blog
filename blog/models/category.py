1a06a3c0a33088094231e00769574118d62540ae
Chriford <66867121+chriford@users.noreply.github.com>
2023-03-14 20:43:42 -0400

    Category Table (#4)
    * create the model
    
    * register model
    
    * add conditional statement to the navigation bar
    
    * add other navigation links
    
    * create post model
    
    * register post model
    
    * generate a migration file
    
    * update a model
    
    * configure for swagger ui
    
    * increase the token lifespan
    
    * modify settings

---
+++ blog/models/category.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _ # noqa
+from blog.models.timestamp import (
+    Timestamp,
+)
+
+class Category(Timestamp):
+    name = models.CharField(
+        max_length=200,
+        verbose_name=_("Name"),
+        null=True,
+        blank=False,
+    )
+    
+    
+    def __str__(self):
+        return self.name
+        
