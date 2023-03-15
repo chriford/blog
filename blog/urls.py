80e40a27e2eb368efc18649609e48ae2d8841906
chriford <siamechrif@gmail.com>
2023-03-15 03:37:14 -0400

    increase the token lifespan

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .views import (
     posts,
     management
@@ -8,4 +14,6 @@ urlpatterns = [
     path('', posts, name='index'),
     path('management/', management, name='management'),
     path('', posts, name='posts'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
