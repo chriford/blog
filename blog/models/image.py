5dc621e441bb9f0506fa6b83391c1dd92d0cc742
chriford <siamechrif@gmail.com>
2023-03-15 14:28:07 -0400

    create the favorite model

--- blog/models/image.py
+++ blog/models/image.py
@@ -6,13 +6,15 @@ from django.utils.translation import gettext_lazy as _
 
 from blog.models.timestamp import Timestamp
 
-class Image(Timestamp):
+class Image(Timestamp): 
     file = models.ImageField(
+        verbose_name=_("Head Image"),
         upload_to='%D-%M-%Y/blog-images',
         null=True,
         blank=False,
     )
     post = models.ForeignKey(
+        verbose_name=_("Post"),
         to='blog.Post',
         on_delete=models.CASCADE,
         null=True,
