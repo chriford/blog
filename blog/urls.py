2baf940d93f51be46a04dd114495589110b44d39
chriford <siamechrif@gmail.com>
2023-03-15 18:59:37 -0400

    register a view

--- blog/urls.py
+++ blog/urls.py
@@ -7,13 +7,15 @@ from rest_framework_simplejwt.views import (
 
 from .views import (
     posts,
-    management
+    management,
+    post_create,
 )
 app_name = 'blog'
 urlpatterns = [
     path('', posts, name='index'),
     path('management/', management, name='management'),
-    path('', posts, name='posts'),
+    path('post/view/', posts, name='posts'),
+    path('post/create/', post_create, name='post-create'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
