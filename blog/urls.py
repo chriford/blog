ce53da007bba803c420068ccaaf6d207652dcd82
chriford <siamechrif@gmail.com>
2023-03-15 19:04:06 -0400

    create and register the post delete view

--- blog/urls.py
+++ blog/urls.py
@@ -9,13 +9,19 @@ from .views import (
     posts,
     management,
     post_create,
+    post_delete,
+    post_update,
 )
+
 app_name = 'blog'
 urlpatterns = [
     path('', posts, name='index'),
     path('management/', management, name='management'),
     path('post/view/', posts, name='posts'),
     path('post/create/', post_create, name='post-create'),
+    path('post/<str:title>/delete/blog/<int:pk>/', post_delete, name='post-delete'),
+    path('post/<str:title>/update/blog/<int:pk>/', post_update, name='post-update'),
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- blog/views/posts.py
+++ blog/views/posts.py
@@ -26,7 +26,7 @@ def post_create(request):
             post.cleaned_data['owner'] = request.user
             post.save(commit=True)
             return redirect('blog:post-create')
-            
+
     posts = Post.objects.all()
     context = {
         'post_form': PostForm,
@@ -42,6 +42,15 @@ def posts(request):
         'posts': posts,
     }
     return render(request, 'blog/posts.html', context)
+
+
+# post_update
+def post_delete(request, title, pk):
+    context = {}
+    return HttpResponse(f"{title}-pk will be deleted")
+# def management(request):
+#     context = {}
+#     return render(request, 'blog/management.html', context)
 
 
 def management(request):
