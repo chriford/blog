5a4724b4e2df651e0ab01c645fe35de74e1131f0
chriford <siamechrif@gmail.com>
2023-03-17 18:56:47 -0400

    style the 404 page

--- security/views/not_found.py
+++ security/views/not_found.py
@@ -11,6 +11,7 @@ def error_view(request, exception):
 def not_found_page_view(request, *args, **kwargs):
     context = {
         'request': request,
+        'prevous_url': request. META,
         'msg': kwargs,
         'layout_path': 'layout/base.html',
     }
