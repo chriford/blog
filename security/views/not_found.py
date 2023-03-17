ca49b093757d992ffba07f946ea7e4507d48a8f4
chriford <siamechrif@gmail.com>
2023-03-17 16:37:55 -0400

    insert banners

---
+++ security/views/not_found.py
@@ -0,0 +1,17 @@
+from django.urls import reverse_lazy
+from django.shortcuts import (
+    HttpResponse,
+    render,
+    redirect,
+)
+
+def error_view(request, exception):
+    return redirect('error_page')
+
+def not_found_page_view(request, *args, **kwargs):
+    context = {
+        'request': request,
+        'msg': kwargs,
+        'layout_path': 'layout/base.html',
+    }
+    return render(request, 'auth/pages/404.html', context)
