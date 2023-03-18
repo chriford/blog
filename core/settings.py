af633212c80b9c5afe06f0ad4081229f1f5a0b54
chriford <siamechrif@gmail.com>
2023-03-18 11:38:11 -0400

    re-configure ngnix

--- core/settings.py
+++ core/settings.py
@@ -222,7 +222,13 @@ CACHES = {
 }
 
 SELECT2_CACHE_BACKEND = "select2"
+# ELASTICSEARCH_DSL={
+#     'default': {
+#         'hosts': 'localhost:9200'
+#     },
+# }
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
