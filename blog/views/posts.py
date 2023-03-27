1961b4c851445cfcfa1f624499d16f53a9f541d8
chriford <siamechrif@gmail.com>
2023-03-27 21:35:44 +0200

    add data types to parameters and modify forms

--- blog/views/posts.py
+++ blog/views/posts.py
@@ -9,9 +9,7 @@ from django.shortcuts import (
     render,
     HttpResponse,     
     redirect,
-    HttpResponseRedirect,
 )
-
 from blog.models import (
     Post,
     Trash,
@@ -35,16 +33,25 @@ def model_form_data_create(request, form_arg: str):
                 post = post_create_form.save(commit=True)
                 post.owner = request.user
                 post.save()
+                messages.success("New blog created successfully")
                 return redirect('blog:post-forms-page')
-
+            else:
+                messages.error("Blog creation failed due to invalid blog form")
+                return redirect('blog:post-forms-page')
+                
         elif form_arg == 'category-create-form':
             category_create_form = CategoryForm(request.POST)
-            # if category_create_form.is_valid(): 
-            category = category_create_form.save(commit=True)
-            category.owner = request.user
-            category.save()
-            return redirect('blog:post-forms-page')
-    return HttpResponse("not a post method")
+            if category_create_form.is_valid(): 
+                category = category_create_form.save(commit=True)
+                category.owner = request.user
+                category.save()
+                messages.success("New category created successfully")
+                return redirect('blog:post-forms-page')
+            else:
+                messages.error("Blog creation failed due to invalid category form")
+                return redirect('blog:post-forms-page')
+
+    return redirect('blog:post-forms-page')
 
 @login_required(login_url=settings.LOGIN_REDIRECT_URL)
 def post_forms_page(request):
@@ -65,6 +72,10 @@ def posts(request):
             user.is_first_time_login = False
             user.save()
             messages.info(f"Welcome to the blog community {user.username}!")
+        else:
+            pass
+    else:
+        pass
     posts = Post.objects.all()
     context = {
         'post_form': PostForm,
@@ -73,7 +84,7 @@ def posts(request):
     }
     return render(request, 'blog/posts.html', context)
 
-def post_view(request, title, pk, *args, **kwargs):
+def post_view(request, title: str, pk: int, *args, **kwargs):
     post_obj = Post.objects.filter(
         title=title,
     ).get(pk=pk)
@@ -84,27 +95,69 @@ def post_view(request, title, pk, *args, **kwargs):
     return render(request, 'blog/post-view.html', context)
 
 
-def post_delete(request, title, pk, *args, **kwargs):
+def post_delete(request, title: str, pk: int, status: str, *args, **kwargs):
     post  = Post.objects.filter(
         pk=pk,
         title=title,
     ).first()
-    post.is_deleted = True
-    post.save(
-        update_fields=(
-            'is_active',
-            'is_deleted',
-            'delete_on',
+    if status == 'temporal':
+        post.is_deleted = True
+        post.save(
+            update_fields=(
+                'is_active',
+                'is_deleted',
+                'delete_on',
+            )
         )
-    )
-    return HttpResponse(f"{title}-pk will be deleted")
+        messages.success(f"Blog post disabled successfully!")
+        return redirect('blog:post-forms-page') # should redirect to the blog management page
+    
+    elif status == 'permanet':
+        post.delete()    
+        messages.success(f"Blog post deleted successfully!")
+        return redirect('blog:post-forms-page') # should redirect to the blog management page
 
-def post_update(request, title, pk, *args, **kwargs):
-    post  = Post.objects.filter(
+    else:
+        pass
+    
+def post_update(request, title: str = None, pk: int = 0, form_arg: str = None, *args, **kwargs):
+    blog_post  = Post.objects.filter(
         pk=pk,
         title=title,
-    )
-    return HttpResponse(f"{title}-pk will be updated")
+    ).first()
+    if request.method == 'POST':
+        if form_arg == 'post-update-form':
+            blog_post_form = PostForm(
+                request.POST,
+                request.FILES,
+                instance=blog_post,
+                data=request.data,
+            )
+            if blog_post_form.is_valid():
+                post = blog_post_form.save(commit=True)
+                post.owner = request.user
+                post.save()
+                messages.success("blog post updated successfully")
+                return redirect('blog:post-forms-page')
+            else:
+                messages.error("blog post creation failed due to invalid blog post form")
+                return redirect('blog:post-forms-page')
+
+        elif form_arg == 'category-update-form':
+            category_update_form = CategoryForm(request.POST)
+            if category_update_form.is_valid(): 
+                category = category_update_form.save(commit=True)
+                category.owner = request.user
+                category.save()
+                messages.success("category updated successfully")
+                return redirect('blog:post-forms-page')
+            else:
+                messages.error("category update failed due to invalid category form")
+                return redirect('blog:post-forms-page')
+        else:
+            pass
+    messages.info("redirected from forbidden page")
+    return redirect('blog:post-forms-page') # should redirect to the blog management page
 
 def management(request):
     context = {}
