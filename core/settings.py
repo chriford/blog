ca133a505163753ba03053f089058652e828a339
Chriford <66867121+chriford@users.noreply.github.com>
2023-03-27 11:08:33 -0400

    Display category form (#29)
    * commit cached files
    
    * update an init file
    
    * create a django messages include template
    
    * include form in a template
    
    * implement and refactor views
    
    * remove some code
    
    * fix broken button
    
    * do a log bunch of changes
    
    * refactor form methods
    
    * update forms
    
    * enable category creation
    
    * make field unique

--- core/settings.py
+++ core/settings.py
@@ -1,11 +1,7 @@
 import os
 from pathlib import Path
 from datetime import timedelta
-import environ
 
-env = environ.Env(
-    DEBUG=(bool, False),
-)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # environ.Env.read_env(BASE_DIR/'.env')
