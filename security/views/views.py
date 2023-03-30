c68eeeff4e29d0d24222776f34f99f9719191beb
chriford <siamechrif@gmail.com>
2023-03-30 10:13:10 +0200

    organize the email sending feature

--- security/views/views.py
+++ security/views/views.py
@@ -8,7 +8,9 @@ from django.core.mail import EmailMessage, get_connection
 
 from security.models import User, Profile
 from security.forms import UserCreationForm
-
+from blog_utils import (
+    send_email,
+) 
 def signup(request):
     if request.method == 'POST':
         user_create_form = UserCreationForm(request.POST)
@@ -54,25 +56,13 @@ def profile(request):
 
 def password_reset(request):
     if request.method == "POST": 
-        # with get_connection(host=settings.EMAIL_HOST, port=settings.EMAIL_PORT,  username=settings.EMAIL_HOST_USER, password=settings.EMAIL_HOST_PASSWORD, use_tls=settings.EMAIL_USE_TLS) as connection:  
-        subject = 'CODE'
-        email_from = settings.EMAIL_HOST_USER
-        email_to = settings.EMAIL_HOST_USER
-        recipient_list = [email_to]  
-        message = 'request.POST.get("message") script sent from the blog app'  
-        send_mail(
-            subject, 
-            message, 
-            email_from, 
-            recipient_list, 
-            fail_silently=False,
+        send_email(
+            recipient_list=[settings.EMAIL_HOST_USER],
+            subject="BLOG - Reset Password",
+            body="Click the link below to reset your password\n www.google.com",    
         )
-        msg = EmailMessage(subject,
-            message, 
-            to=recipient_list)
-        msg.send()
-    
-        return redirect('blog:posts')
-    
+        messages.success(request, "We have sent an email to the email you provided on registration to this platform")
+        return redirect('security:forgot-password')
+
     context = {}
     return render(request, 'auth/forgot_password.html', context)
