5a83f972388712a6e38e2fbc625de4eea285e6ef
chriford <siamechrif@gmail.com>
2023-04-02 20:04:20 +0200

    change meta data for comments model

--- core/settings.py
+++ core/settings.py
@@ -98,6 +98,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': 'blogger',
+        # 'HOST': 'localhost',
+        # 'PORT': 5432,
+        # 'PASSWORD': 'digi.Brands7.',
     }
 }
 
