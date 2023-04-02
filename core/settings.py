5a679bd91a244a750f906677ed63e3e1ec9516c4
chriford <siamechrif@gmail.com>
2023-04-02 20:59:47 +0200

    comment code

--- core/settings.py
+++ core/settings.py
@@ -98,10 +98,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        # 'USER': 'blogger',
-        # 'HOST': 'localhost',
-        # 'PORT': 5432,
-        # 'PASSWORD': 'digi.Brands7.',
+        # 'USER': 'blog',
+        # 'HOST': '0.0.0.0',
+        # 'PORT': "5432",
+        # 'PASSWORD': 'digibrand-blog',
     }
 }
 
