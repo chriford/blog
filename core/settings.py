85a03a08fd7d40abe81f818f379bba5bbb74f0a1
chriford <siamechrif@gmail.com>
2023-04-08 08:18:28 +0200

    refactor a dir

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "blog/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "blog/static-files")]
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "/media/"
