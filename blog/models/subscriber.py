f75e0d2b356727955899cbc4c57e015f72162a08
chriford <siamechrif@gmail.com>
2023-04-21 23:48:10 +0200

    create the subscriber model

---
+++ blog/models/subscriber.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from blog.models.category import Category
+from blog.models.timestamp import Timestamp
+
+
+class Subscriber(Timestamp):
+    user = models.ForeignKey(
+        "security.User",
+        related_name="user",
+        help_text=_("Subscriber to newsletter"),
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    is_subscribed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return self.user.username
